d0728b24ebfd01ae40951787eff36a18b13a6a46
tom8u4286 <tom8u4286@gmail.com>
2022-12-12 14:01:28 +0800

    修改 以兩鏈結逐節點方式相加方法(LeetCode Accept)

--- 2.AddTwoNumbers.M.playground/Contents.swift
+++ 2.AddTwoNumbers.M.playground/Contents.swift
@@ -11,13 +11,116 @@ public class ListNode {
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         
-        // 取得兩條ListNode的數字 並相加
-        let sum = getListNumber(l1) + getListNumber(l2)
+        /** 確認l1與l2都不是nil，
+         * 1. 如果皆為nil，return nil
+         * 2. 如果其一為nil，則return另一
+         * 3. 如果皆有值，才開始進行運算。
+         */
+        if l1 == nil && l2 == nil {return nil}
+        if l1 == nil { return l2 }
+        if l2 == nil { return l1 }
         
-        // 將數字轉換回ListNode
-        let result = intToList(num: sum)
+        /** 通過上面的檢查，表示兩ListNode皆有值，不是空的Link list。
+         */
+        var newList: ListNode?
+        // 在第一個節點被創建時，要記錄第一個Node，於運算結束後回傳。
+        var headList: ListNode?
         
-        return result
+        var tmpL1 = l1
+        var tmpL2 = l2
+        // 進位
+        var carry = false
+        
+        while(true){
+            var value = tmpL1!.val + tmpL2!.val
+            // 如果前一個節點相加有進位，此處加上進位的1。
+            if carry {
+                value += 1
+                carry = false
+            }
+            
+            // 如果數值超過10，進位
+            if(value > 9) {
+                carry = true
+                value %= 10
+            }
+            // 以此value建構一個新Node
+            let tmpNode = ListNode(value)
+            
+            // 如果newList還沒有值，表示這個node是第一個節點
+            if newList == nil {
+                newList = tmpNode
+                headList = tmpNode
+                
+            /** 如果newList有值了，將newList當前節點的next點指向tmpNode。
+             * 再將ListNode指向tmpNode。
+             */
+            } else {
+                newList?.next = tmpNode
+                newList = tmpNode
+            }
+            
+            /** 如果兩鏈結都沒有下一個節點，
+             * 表示運算結束，離開while迴圈。
+             */
+            if(tmpL1?.next == nil && tmpL2?.next == nil){
+                /** 如果在兩鏈結都結尾時，仍有一個進位，
+                 * 新增一個value為1的節點接在最後。
+                 */
+                if(carry){
+                    newList?.next = ListNode(1)
+                }
+                break
+            }
+            
+            /** 如果其一鏈結為nil，
+             * 1. 先檢查是否有進位，有進位的話要再新增一個value為1的節點，給下一輪運算。
+             * 2. 如果沒有進位，表示運算結束。將newList的next指向另一鏈結的next。
+             */
+            if(tmpL1?.next == nil){
+                // 如果還有進位
+                if(carry){
+                    tmpL1?.next = ListNode(1)
+                    tmpL1 = tmpL1?.next
+                    tmpL2 = tmpL2?.next
+                    /** 在某節點結尾時，仍有進位的狀況較特殊，
+                     * 比須將carry設定為false，否則將會在下一輪的carry判斷中，又再加一次進位的1。
+                     */
+                    carry = false
+                    continue
+                }
+                /** 如果沒有進位，表示所有運算已經完結，
+                 * 將newList的next指向另一鏈結的next。
+                 */
+                newList?.next = tmpL2?.next
+                break
+            }
+            if(tmpL2?.next == nil){
+                // 如果還有進位
+                if(carry){
+                    tmpL2?.next = ListNode(1)
+                    tmpL1 = tmpL1?.next
+                    tmpL2 = tmpL2?.next
+                    /** 在某節點結尾時，仍有進位的狀況較特殊，
+                     * 比須將carry設定為false，否則將會在下一輪的carry判斷中，又再加一次進位的1。
+                     */
+                    carry = false
+                    continue
+                }
+                /** 如果沒有進位，表示所有運算已經完結，
+                 * 將newList的next指向另一鏈結的next。
+                 */
+                newList?.next = tmpL1?.next
+                break
+            }
+            
+            // 如果tmpL1與tmpL2的next都有還有節點，則兩LinkList都指向下一節點
+            tmpL1 = tmpL1?.next
+            tmpL2 = tmpL2?.next
+            
+        }
+        
+        return headList
     }
     
     // 將List轉換為Int數字
@@ -108,8 +211,5 @@ while(node != nil){
     print(node?.val)
     node = node?.next
 }
-//sol.getListNumber(node11)
-//sol.getListNumber(node21)
 
-//let test = Int(pow(10.0, 2.0))
-//123%10
+
